ad64d88c9535c40958022e956856cde8148064b7
Slava <krylov_s10@mail.ru>
2025-01-10 01:04:09 +0700

    fix/5

---
+++ src/services/InvitationService.ts
@@ -0,0 +1,42 @@
+import { api } from "../api/api";
+import type { Invitation, InvitationRequest } from "../types/invitationTypes";
+import { AuthServiceError } from "../error/ServiceError";
+
+const InvitationService = {
+  async getAll(): Promise<Invitation[]> {
+    try {
+      const response = await api.get("/invitations");
+      return response.data;
+    } catch (error) {
+      throw new AuthServiceError(
+        error.response?.data?.message || "Ошибка при отправке приглашения",
+        error.response?.status,
+      );
+    }
+  },
+
+  async send(invitationData: InvitationRequest): Promise<void> {
+    try {
+      await api.post("/invitations/send", invitationData);
+    } catch (error) {
+      throw new AuthServiceError(
+        error.response?.data?.message || "Ошибка при отправке приглашения:",
+        error.response?.status,
+      );
+    }
+  },
+
+  async confirm(token: string): Promise<void> {
+    try {
+      await api.post(`/invitations/${token}/confirm`);
+    } catch (error) {
+      throw new AuthServiceError(
+        error.response?.data?.message ||
+          "Ошибка при подтверждении приглашения с токеном ${token}",
+        error.response?.status,
+      );
+    }
+  },
+};
+
+export default InvitationService;
